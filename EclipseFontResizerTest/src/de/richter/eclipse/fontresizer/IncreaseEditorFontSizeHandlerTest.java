63f6742d24a78418dafc3b29770ba62a1da2bc08
Ingo Richter <ingo.richter@gmail.com>
2013-01-05 21:08:09 -0800

    - fixed broken test case - added pom.xml

--- EclipseFontResizerTest/src/de/richter/eclipse/fontresizer/IncreaseEditorFontSizeHandlerTest.java
+++ EclipseFontResizerTest/src/de/richter/eclipse/fontresizer/IncreaseEditorFontSizeHandlerTest.java
@@ -1,25 +1,23 @@
 /*
- * Copyright 2011 Ingo Richter 
- * 
+ * Copyright 2011 Ingo Richter
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at 
- * 
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *  
+ *
  */
 package de.richter.eclipse.fontresizer;
 
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.IPropertyChangeListener;
-import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.internal.Workbench;
 import org.junit.Before;
 import org.junit.Test;
@@ -33,20 +31,17 @@ public class IncreaseEditorFontSizeHandlerTest {
 	}
 
 	@Test
-	public void testExecute() throws ExecutionException {
-		
-		
+	public void testExecute() throws org.eclipse.core.commands.ExecutionException {
 		IPreferenceStore preferenceStore = Workbench.getInstance().getPreferenceStore();
 		preferenceStore.addPropertyChangeListener(new IPropertyChangeListener() {
-			
 			@Override
-			public void propertyChange(PropertyChangeEvent event) {
+			public void propertyChange(org.eclipse.jface.util.PropertyChangeEvent event) {
 				System.err.println("PCE Fired");
 				String property= event.getProperty();
 				System.err.println(property);
 			}
 		});
-		
+
 		IncreaseEditorFontSizeHandler editorFontSizeHandler = new IncreaseEditorFontSizeHandler();
 		editorFontSizeHandler.execute(null);
 		// check if the font-size was increased by one
